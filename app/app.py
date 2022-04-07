1728f7338b7237764c461f2d8b0df3653158a86c
Filipe Galelli Scaravonatto <72172186+lpegs@users.noreply.github.com>
2022-04-07 17:28:28 -0300

    added charts n proper db conn
    added charts in html
    added connection not with js but python
    getting close to the end of the project

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from crypt import methods
 from urllib import request
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3 as sql
 import pymysql
 
@@ -12,6 +12,10 @@ cursor = connection.cursor()
 
 app = Flask(__name__)
 
+app.config.update (
+    TEMPLATES_AUTO_RELAOD = True
+)
+
 @app.route("/", methods=["GET", "POST"])
 def home():
     return render_template("index.html")
@@ -19,8 +23,25 @@ def home():
 
 @app.route("/newcase", methods=["GET", "POST"])
 def newcase():
+    
+    totalcount = cursor.execute("SELECT * FROM user_table;")
+    mencount = cursor.execute("SELECT * FROM user_table where gender = 'M';")
+    womencount = cursor.execute("SELECT * FROM user_table where gender = 'F';")
+    othercount = cursor.execute("SELECT * FROM user_table where gender = 'O';")
+    coughcount = cursor.execute("SELECT * FROM user_table where cough = 'yes';")
+    fevercount = cursor.execute("SELECT * FROM user_table where fever = 'yes';")
+    breathcount = cursor.execute("SELECT * FROM user_table where breath = 'yes';")
+    fatiguecount = cursor.execute("SELECT * FROM user_table where fatigue = 'yes';")
+    bodyachecount = cursor.execute("SELECT * FROM user_table where bodyache = 'yes';")
+    headachecount = cursor.execute("SELECT * FROM user_table where headache = 'yes';")
+    throatcount = cursor.execute("SELECT * FROM user_table where throat = 'yes';")
+    runnynosecount = cursor.execute("SELECT * FROM user_table where runnynose = 'yes';")
+    nauseacount = cursor.execute("SELECT * FROM user_table where nausea = 'yes';")
+    
+    connection.commit()
+    
     if request.method == "GET":
-        return render_template("index.html")
+        return render_template("index.html", totalcount=totalcount, mencount=mencount, womencount=womencount, othercount=othercount, coughcount=coughcount, fevercount=fevercount, breathcount=breathcount, fatiguecount=fatiguecount, bodyachecount=bodyachecount, headachecount=headachecount, throatcount=throatcount, runnynosecount=runnynosecount, nauseacount=nauseacount)
     else:
 
         name = request.form["name"]
@@ -41,10 +62,10 @@ def newcase():
             cursor.execute(sql)
             connection.commit()
             
-            return render_template("successfull.html")
+            return render_template("index.html", totalcount=totalcount, mencount=mencount, womencount=womencount, othercount=othercount, coughcount=coughcount, fevercount=fevercount, breathcount=breathcount, fatiguecount=fatiguecount, bodyachecount=bodyachecount, headachecount=headachecount, throatcount=throatcount, runnynosecount=runnynosecount, nauseacount=nauseacount)
             
         except connection.Error as err:
-            return render_template("dberror.html", name=name)
+            return render_template("dberror.html", totalcount=totalcount, mencount=mencount, womencount=womencount, othercount=othercount, coughcount=coughcount, fevercount=fevercount, breathcount=breathcount, fatiguecount=fatiguecount, bodyachecount=bodyachecount, headachecount=headachecount, throatcount=throatcount, runnynosecount=runnynosecount, nauseacount=nauseacount)
         
 if __name__ == "__main__":
     app.run()
