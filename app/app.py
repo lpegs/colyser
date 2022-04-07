7744b7c0604a7fe7145c02fe62a4f76bc62485f4
Filipe Galelli Scaravonatto <72172186+lpegs@users.noreply.github.com>
2022-04-07 13:29:26 -0300

    added flask route
    database connection with flask
    added inputs and selects to the modal

--- app/app.py
+++ app/app.py
@@ -1,11 +1,50 @@
-from flask import Flask, render_template
+from crypt import methods
+from urllib import request
+from flask import Flask, render_template, request
+import sqlite3 as sql
+import pymysql
+
+# connecting to the database
+connection = pymysql.connect (host="localhost", user="root", password="123", database="colyser")
+
+# setting the cursor
+cursor = connection.cursor()
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
     return render_template("index.html")
 
+
+@app.route("/newcase", methods=["GET", "POST"])
+def newcase():
+    if request.method == "GET":
+        return render_template("index.html")
+    else:
+
+        name = request.form["name"]
+        age = request.form["age"]
+        gender = request.form["gender"]
+        cough = request.form["cough"]
+        fever = request.form["fever"]
+        breath = request.form["breath"]
+        fatigue = request.form["fatigue"]
+        bodyache = request.form["bodyache"]
+        headache = request.form["headache"]
+        throat = request.form["throat"]
+        runnynose = request.form["runnynose"]
+        nausea = request.form["nausea"]
+        
+        try:           
+            sql = "INSERT INTO user_table (name, age, gender, cough, fever, breath, fatigue, bodyache, headache, throat, runnynose, nausea) VALUES ('" + name + "', '" + age + "', '" + gender + "', '" + cough + "', '" + fever + "', '" + breath + "', '" + fatigue + "', '" + bodyache + "', '" + headache + "', '" + throat + "', '" + runnynose + "', '" + nausea + "')"
+            cursor.execute(sql)
+            connection.commit()
+            
+            return render_template("successfull.html")
+            
+        except connection.Error as err:
+            return render_template("dberror.html", name=name)
+        
 if __name__ == "__main__":
     app.run()
-    
